74e5e26b2a7a45273386857032987e5713eb2b91
Kevin Smith <kevin@bryghtlabs.com>
2026-07-20 13:35:36 -0500

    timer WIP

--- config.mcc/src/main.c
+++ config.mcc/src/main.c
@@ -34,16 +34,33 @@
 // *****************************************************************************
 // *****************************************************************************
 
+static volatile uint32_t ticksMs;
+
+static void tickISR(TC_TIMER_STATUS status, uintptr_t context) {
+  ticksMs++;
+}
+
+static void delay(uint32_t ms) {
+  const uint32_t endTime = ticksMs + ms + 1;
+
+  while (ticksMs < endTime) {
+  }
+}
+
 int main ( void )
 {
     /* Initialize all modules */
     SYS_Initialize ( NULL );
+
+    TC0_TimerCallbackRegister(tickISR, (uintptr_t)NULL);
 
     while ( true )
     {
         LED_CH_EN_Toggle();
         /* Maintain state machines of all polled MPLAB Harmony modules. */
         SYS_Tasks ( );
+
+        delay(1);
     }
 
     /* Execution should not come here during normal operation */
